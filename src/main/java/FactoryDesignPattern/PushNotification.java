527b98c4257cc736fed6fb1d96bf859403ec093c
agrawal101 <gauravagrawal22499@gmail.com>
2025-04-26 23:23:33 +0530

    FactoryDesignPattern

---
+++ src/main/java/FactoryDesignPattern/PushNotification.java
@@ -0,0 +1,9 @@
+package FactoryDesignPattern;
+
+public class PushNotification implements Notification{
+    @Override
+    public void notifyUser()
+    {
+        System.out.println("Sending Push Notification");
+    }
+}
