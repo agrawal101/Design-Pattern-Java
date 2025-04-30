c40620d2605acc20c14b0c41641f477cb13f93dd
agrawal101 <gauravagrawal22499@gmail.com>
2025-04-30 23:09:00 +0530

    ProxyDesignPattern

---
+++ src/main/java/ProxyDesignPattern/Main.java
@@ -0,0 +1,13 @@
+package ProxyDesignPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Image img1 = new ProxyImage("Image1.png");
+        Image img2 = new ProxyImage("Image2.png");
+        img1.display();
+        img2.display();
+        img1.display();
+    }
+
+}
